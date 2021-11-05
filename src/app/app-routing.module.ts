a91017e2adfaf51640869f53760e319b1108e8d6
harsha31024 <aharsha6@gmail.com>
2021-11-05 12:44:58 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { ViewAllProductsComponent} from './view-all-products/view-all-products.component';
 import { CactivateGuard} from './cactivate.guard';
 import { SingleProductComponent} from './single-product/single-product.component';
-import { ViewproductsByCategoryComponent } from './viewproducts-by-category/viewproducts-by-category.component';
+// import { ViewproductsByCategoryComponent } from './viewproducts-by-category/viewproducts-by-category.component';
 import { GooglemapsComponent} from './googlemaps/googlemaps.component';
 
 
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path:"products",component:ViewAllProductsComponent,canActivate:[CactivateGuard]},
   { path: "login", component: LoginComponent},
   { path: "products/:id", component:SingleProductComponent,canActivate:[CactivateGuard]},
-  { path: "productbycategory/:id",component:ViewproductsByCategoryComponent},
+  // { path: "productbycategory/:id",component:ViewproductsByCategoryComponent},
   { path: "googlemaps",component:GooglemapsComponent}
   
 ];
